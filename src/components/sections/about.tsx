1df992f138010444e93233598dce1a4955c5259f
emeka mamah <mamahemeka84@gmail.com>
2025-04-04 20:53:21 +0100

    update

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import profileImage from '../../../images/image.png';
+import profileImage from '../../../images/homepage/image.png';
 
 import { shimmer, toBase64 } from '@/lib/utils';
 
